0206ed3d3d11b88bcaa253a5b807f229793f56e9
tomyanceyGramercy <65623232+tomyanceyGramercy@users.noreply.github.com>
2024-10-28 13:17:28 -0700

    hello commit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Yo from './components/Yo.vue';
+import confetti from 'canvas-confetti';
+confetti();
 </script>
 
 <template>
-    <div>
-        <h1>Welcome to jeremyrepo</h1>
-        <HelloWorld msg="You did it!" />
-    </div>
+  <div>
+    <h1>Welcome to jeremyrepo so</h1>
+    <HelloWorld msg="You did it!" />
+    <Yo msg="From YO!" />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
